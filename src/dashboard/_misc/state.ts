3c31574fecc1dabdf4858ba3f27847ff44835038
oliver <o-lohr@gmx.de>
2019-08-14 16:16:40 +0200

    Add tracker stuff and nodecg speedcontrol stuff to Vuex state

--- src/dashboard/_misc/state.ts
+++ src/dashboard/_misc/state.ts
@@ -2,7 +2,8 @@ import clone from 'clone';
 import { ReplicantBrowser } from 'nodecg/types/browser'; // eslint-disable-line
 import Vue from 'vue';
 import Vuex from 'vuex';
-import { Bingoboard, BingoboardMeta, BingosyncSocket } from "../../../schemas";
+import { Bingoboard, BingoboardMeta, BingosyncSocket, DonationTotal, TrackerOpenBids, TrackerDonations } from "../../../schemas";
+import { RunDataActiveRun, RunDataArray } from "../../../speedcontrol-types";
 
 Vue.use(Vuex);
 
@@ -10,14 +11,28 @@ const replicantNames = [
   'bingoboard',
   'bingoboardMeta',
   'bingosyncSocket',
+  'donationTotal',
+  'trackerDonations',
+  'trackerOpenBids',
 ];
+const nodecgSpeedcontrolReplicantNames = [
+  'runDataActiveRun',
+  'runDataArray',
+]
 const replicants: ReplicantBrowser<unknown>[] = [];
 
 export const store = new Vuex.Store({
   state: {
+    // bingothon
     bingoboard: [] as Bingoboard,
     bingoboardMeta: {} as BingoboardMeta,
     bingosyncSocket: {} as BingosyncSocket,
+    donationTotal: 0 as DonationTotal,
+    trackerDonations: [] as TrackerDonations,
+    trackerOpenBids: [] as TrackerOpenBids,
+    // nodecg-speedcontrol
+    runDataActiveRun: {} as RunDataActiveRun,
+    runDataArray: [] as RunDataArray,
   },
   mutations: {
     updateReplicant(state, { name, value }) {
@@ -38,6 +53,19 @@ replicantNames.forEach((name) => {
 
   replicants.push(replicant);
 });
+
+nodecgSpeedcontrolReplicantNames.forEach(name => {
+  const rep = nodecg.Replicant(name, 'nodecg-speedcontrol');
+
+  rep.on('change', newVal => {
+    store.commit('updateReplicant', {
+      name: rep.name,
+      value: clone(newVal),
+    });
+  });
+
+  replicants.push(rep);
+})
 
 export async function create() {
   return NodeCG.waitForReplicants(...replicants).then(() => store);
