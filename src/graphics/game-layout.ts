504aee1948bda66b73d371fbdf401b3987e8c4e0
oliver <o-lohr@gmx.de>
2019-08-17 19:11:20 +0200

    Added layout stub

---
+++ src/graphics/game-layout.ts
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import GameLayout from './game-layout/layout1.vue';
+
+new Vue({
+  el: '#App',
+  render: h => h(GameLayout),
+});
---
+++ src/graphics/game-layout/layout1.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="App">
+      Hello World
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Watch, Prop } from "vue-property-decorator";
+import { nodecg, NodeCG } from "../../browser-util/nodecg";
+import { Bingoboard, BingosyncSocket, BingoboardMeta } from "../../../schemas";
+import { store, getReplicant } from "../../browser-util/state";
+
+@Component({})
+export default class GameLayout extends Vue {
+
+}
+</script>
+
+<style>
+
+</style>
