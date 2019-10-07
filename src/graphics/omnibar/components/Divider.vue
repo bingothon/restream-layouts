4eaa1ce44f7654ab41fed8f0833f9c9255108b6f
Floha <floha2011@hotmail.de>
2019-10-07 20:26:10 +0200

    I have no idea, if this is working or not

---
+++ src/graphics/omnibar/components/Divider.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="Divider" />
+</template>
+
+<script lang="ts">
+export default class Divider extends Vue{};
+</script>
+
+<style scoped>
+  .Divider {
+    height: 100%;
+    width: 3px;
+    min-width: 3px;
+    background-color: #fff073;
+  }
+</style>
---
+++ src/graphics/omnibar/components/DonationTotal.vue
@@ -0,0 +1,78 @@
+<template>
+  <div
+    id="Total"
+  >
+    <span
+      v-for="char in totalSplitString"
+      :key="char"
+      :class="(char === ',' ? 'Comma' : undefined)"
+    >
+      {{ char }}
+    </span>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop} from "vue-property-decorator";
+import { TweenLite } from "gsap";
+const totalRep = nodecg.Replicant('donationTotal');
+
+export default class DonationTotal extends Vue {
+
+  @Prop({default: false})
+  init: boolean;
+
+  @Prop({default: -1})
+  total: number;
+
+  @Prop({default: 0})
+  tweenedTotal: number;
+  
+  @Prop({default: []})
+  totalSplitString: String;
+
+  watch: {
+  
+    total(val) {
+      if (this.init) {
+        TweenLite.to(this.$data, 5, { tweenedTotal: val });
+      } else {
+        this.tweenedTotal = this.total;
+        this.init = true;
+      }
+    }
+
+    tweenedTotal(val): void {
+      const string = `$${val.toLocaleString('en-US', { maximumFractionDigits: 0 })}`;
+      this.totalSplitString = string.split('');
+    }
+  }
+  
+  mounted() {
+    totalRep.on('change', (newVal: number) => {
+      this.total = newVal;
+    });
+  }
+};
+</script>
+
+<style scoped>
+  #Total {
+    padding: 0 10px;
+    font-size: 40px;
+    font-weight: 500;
+    min-width: 50px;
+    text-align: center;
+  }
+  /* Each character in the total is in a span; setting width so the numbers appear monospaced. */
+  #Total > span {
+    display: inline-block;
+    width: 0.45em;
+    text-align: center;
+  }
+  #Total > .Comma {
+    display: inline-block;
+    width: 0.22em;
+    text-align: center;
+  }
+</style>
