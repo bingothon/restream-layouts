826d44fc4630a54d1adec9afc6181dcc8eb639c7
Floha <floha2011@hotmail.de>
2019-11-24 13:13:21 +0100

    4p 4x3 is now actually 4x3 and not a weird 16x9

--- src/graphics/game-layout/4_3-4p.vue
+++ src/graphics/game-layout/4_3-4p.vue
@@ -1,20 +1,24 @@
 <template>
 	<div>
-    <div id="fillmiddle" class="flexContainer"></div>
+    <div id="fillleft" class="flexContainer"></div>
+    <div id="fillright" class="flexContainer"></div>
 		<twitch-player id="stream1" streamIndex="0"></twitch-player>
 		<twitch-player id="stream2" streamIndex="1"></twitch-player>
 		<twitch-player id="stream3" streamIndex="2"></twitch-player>
 		<twitch-player id="stream4" streamIndex="3"></twitch-player>
     <div id="fillbar" class="flexContainer"></div>
     <div id="fillvoice" class="flexContainer"></div>
-    <player-info id="pi1" playerIndex="0" height=45px></player-info>
-    <player-info id="pi2" playerIndex="1" height=45px></player-info>
-    <player-info id="pi3" playerIndex="2" height=45px></player-info>
-    <player-info id="pi4" playerIndex="3" height=45px></player-info>
+		<div id="fillupperplayers"></div>
+		<player-info id="pi1" playerIndex="0" height=45px></player-info>
+		<player-info id="pi2" playerIndex="1" height=45px></player-info>
+		<div id="filllowerplayers"></div>
+		<player-info id="pi3" playerIndex="2" height=45px></player-info>
+		<player-info id="pi4" playerIndex="3" height=45px></player-info>
 		<test-game-container id="game"></test-game-container>
     <test-timer-container id="timer"></test-timer-container>
-    <bingo-board id="Bingo-board" fontSize="20px"></bingo-board>
-    <discord-voice-display id="discord-voice" iconHeight="40px" nameWidth="114px"/>
+    <bingo-board id="Bingo-board" fontSize="30px"></bingo-board>
+
+    <discord-voice-display id="discord-voice" iconHeight="40px" nameWidth="125px"></discord-voice-display>
 	</div>
 </template>
 
@@ -30,8 +34,8 @@
   import TeamInfo from "../components/teamInfo.vue";
   import PlayerTeamContainer from "../components/playerTeamContainer.vue";
   import DiscordVoiceDisplay from "../components/discordVoiceDisplay.vue";
-  import { RunDataPlayer, RunDataTeam } from "../../../speedcontrol-types";
   import TwitchPlayer from "../components/twitchPlayer.vue";
+  import { RunDataPlayer, RunDataTeam } from "../../../speedcontrol-types";
 
 	@Component({
 		components: {
@@ -42,53 +46,51 @@
       PlayerTeamContainer,
       TestTimerContainer,
       DiscordVoiceDisplay,
-			TwitchPlayer
+      TwitchPlayer,
 		}
 	})
 
 	export default class GameLayout extends Vue {
-    get teams(): RunDataTeam[] {
-      return store.state.runDataActiveRun.teams;
-    }
+
 	}
 </script>
 
 <style scoped>
   #stream1 {
     position: absolute;
-    top: 60px;
+    top: 0px;
     left: 0px;
-    width: 782px;
-    height: 440px;
+    width: 667px;
+    height: 500px;
     background-image: url("../../../static/middle-info-background.png");
-    border: 2px blue solid;
+    border: 2px var(--container-border-color) solid;
   }
   #stream2 {
     position: absolute;
-    top: 60px;
-    left: 1138px;
-    width: 782px;
-    height: 440px;
+    top: 0px;
+    left: 1253px;
+    width: 667px;
+    height: 499px;
     background-image: url("../../../static/middle-info-background.png");
-    border: 2px blue solid;
+    border: 2px var(--container-border-color) solid;
   }
   #stream3 {
     position: absolute;
     top: 500px;
     left: 0px;
-    width: 782px;
-    height: 440px;
+    width: 667px;
+    height: 500px;
     background-image: url("../../../static/middle-info-background.png");
-    border: 2px blue solid;
+    border: 2px var(--container-border-color) solid;
   }
   #stream4 {
     position: absolute;
     top: 500px;
-    left: 1138px;
-    width: 782px;
-    height: 440px;
+    left: 1253px;
+    width: 667px;
+    height: 499px;
     background-image: url("../../../static/middle-info-background.png");
-    border: 2px blue solid;
+    border: 2px var(--container-border-color) solid;
   }
   #fillbar {
     position: absolute;
@@ -97,85 +99,132 @@
     width: 1920px;
     height: 80px;
     background-image: url("../../../static/middle-info-background.png");
-    border: 2px blue solid;
+    border: 2px var(--container-border-color) solid;
   }
-  #fillvoice {
+  #discord-voice {
     position: absolute;
-    top: 350px;
-    left: 782px;
-    width: 356px;
-    height: 150px;
+	top: 125px;
+	left: 1056px;
+	width: 194px;
+	height: 332px;
     background-image: url("../../../static/middle-info-background.png");
-    border: 2px blue solid;
+    border: 2px var(--container-border-color) solid;
   }
-  #fillmiddle {
-    background-image: url("../../../static/middle-info-background.png");
+  #InfoStorageBox1 {
     position: absolute;
+    left: 626px;
     top: 0px;
-    left: 782px;
-    width: 356px;
-    border: 2px blue solid;
-    height: 1080px;
+    width: 668px;
+    height: 308px;
+  }
+  .PlayerContainer {
+    width: 580px;
+  }
+  .PlayerContainer >>> .PlayerInfoBox {
+    height: 40px;
+    font-size: 25px;
+  }
+  #PlayerContainer1, #PlayerContainer3 {
+    margin-right: 88px;
+  }
+  #PlayerContainer2, #PlayerContainer4 {
+    margin-top: 3px;
+    margin-left: 88px;
+  }
+  #InfoStorageBox2 {
+    position: absolute;
+    left: 626px;
+    top: 633px;
+    width: 668px;
+    height: 307px;
   }
   #pi1 {
     position: absolute;
     top: 0px;
-    left: 0px;
-    border: 2px blue solid;
-    width: 769px;
+    left: 670px;
+    border: 2px var(--container-border-color) solid;
+    width: 530px;
   }
+
   #pi2 {
-    position: absolute;
-    top: 0px;
-    left: 1138px;
-    border: 2px blue solid;
-    width: 769px;
+	  position: absolute;
+	  top: 63px;
+	  left: 705px;
+	  border: 2px var(--container-border-color) solid;
+	  width: 530px;
   }
   #pi3 {
     position: absolute;
-    top: 940px;
-    left: 0px;
-    border: 2px blue solid;
-    width: 769px;
+    top: 875px;
+    left: 670px;
+    border: 2px var(--container-border-color) solid;
+    width: 530px;
   }
+
   #pi4 {
-    position: absolute;
-    top: 940px;
-    left: 1138px;
-    border: 2px blue solid;
-    width: 769px;
+	  position: absolute;
+	  top: 938px;
+	  left: 705px;
+	  border: 2px var(--container-border-color) solid;
+	  width: 530px;
   }
   #Bingo-board {
     position: absolute;
-    top: 500px;
-    left: 782px;
-    border: 2px blue solid;
-    width:356px;
-    height:440px;
+    top: 460px;
+    left: 667px;
+    border: 2px var(--container-border-color) solid;
+    width:584px;
+    height:412px;
   }
   #game {
     background-image: url("../../../static/middle-info-background.png");
     position: absolute;
-    top: 60px;
-    left: 782px;
-    width: 356px;
-    border: 2px blue solid;
-    height: 145px;
+    top: 125px;
+    left: 667px;
+    width: 386px;
+    border: 2px var(--container-border-color) solid;
+    height: 165px;
+  }
+  #fillleft {
+    position: absolute;
+    top: 0px;
+    height: 1080px;
+    left: 0px;
+    width: 660px;
+    background-image: url("../../../static/middle-info-background.png");
+    border: 2px var(--container-border-color) solid;
+  }
+  #fillright {
+    position: absolute;
+    top: 0px;
+    height: 1080px;
+    left: 1260px;
+    width: 660px;
+    background-image: url("../../../static/middle-info-background.png");
+    border: 2px var(--container-border-color) solid;
   }
   #timer{
     position: absolute;
-    top: 205px;
-    left: 782px;
+    top: 293px;
+    left: 667px;
     background-image: url("../../../static/middle-info-background.png");
-    width: 356px;
-    border: 2px blue solid;
-    height: 145px;
+    width: 386px;
+    border: 2px var(--container-border-color) solid;
+    height: 165px;
   }
-  #discord-voice {
-    position: absolute;
-    top: 354px;
-    left: 785px;
-    width: 351px;
-    height: 142px;
+  #fillupperplayers, #filllowerplayers {
+	  background-image: linear-gradient(var(--alternative-main-color), var(--darker-main-color));
+	  left: 672px;
+	  height: 130px;
+	  width: 581px;
+	  position: absolute;
   }
+
+  #fillupperplayers {
+	  top: 0px;
+  }
+  #filllowerplayers {
+	  top: 870px;
+  }
+
 </style>
