1316c969ec079f768a0030ae22ca91daed169e2e
Floha <floha2011@hotmail.de>
2020-05-04 20:50:48 +0200

    new layouts p1

---
+++ src/graphics/game-layout/10_9-4p.vue
@@ -0,0 +1,210 @@
+<template>
+	<div>
+    <div id="fillleft" class="flexContainer"></div>
+    <div id="fillright" class="flexContainer"></div>
+		<twitch-player id="stream1" streamIndex="0"></twitch-player>
+		<twitch-player id="stream2" streamIndex="1"></twitch-player>
+		<twitch-player id="stream3" streamIndex="2"></twitch-player>
+		<twitch-player id="stream4" streamIndex="3"></twitch-player>
+    <div id="fillbar" class="flexContainer"></div>
+    <div id="fillvoice" class="flexContainer"></div>
+		<player-info id="pi1" playerIndex="0" height=45px></player-info>
+		<player-info id="pi2" playerIndex="1" height=45px></player-info>
+		<player-info id="pi3" playerIndex="2" height=45px></player-info>
+		<player-info id="pi4" playerIndex="3" height=45px></player-info>
+		<test-game-container id="game"></test-game-container>
+    <test-timer-container id="timer"></test-timer-container>
+    <bingo-board id="Bingo-board" fontSize="30px"></bingo-board>
+
+    <discord-voice-display id="discord-voice" iconHeight="40px" nameWidth="125px"></discord-voice-display>
+	</div>
+</template>
+
+<script lang="ts">
+	import { Component, Vue, Watch, Prop } from "vue-property-decorator";
+	import { nodecg, NodeCG } from "../../browser-util/nodecg";
+	import { Bingoboard, BingosyncSocket, BingoboardMeta } from "../../../schemas";
+	import { store, getReplicant } from "../../browser-util/state";
+  import TestTimerContainer from "../components/timerContainer.vue";
+	import TestGameContainer from "../components/gameContainer.vue";
+  import BingoBoard from "../components/bingoboard.vue";
+  import PlayerInfo from "../components/playerInfo.vue";
+  import TeamInfo from "../components/teamInfo.vue";
+  import PlayerTeamContainer from "../components/playerTeamContainer.vue";
+  import DiscordVoiceDisplay from "../components/discordVoiceDisplay.vue";
+  import TwitchPlayer from "../components/twitchPlayer.vue";
+  import { RunDataPlayer, RunDataTeam } from "../../../speedcontrol-types";
+
+	@Component({
+		components: {
+			BingoBoard,
+      TestGameContainer,
+      PlayerInfo,
+      TeamInfo,
+      PlayerTeamContainer,
+      TestTimerContainer,
+      DiscordVoiceDisplay,
+      TwitchPlayer,
+		}
+	})
+
+	export default class GameLayout extends Vue {
+
+	}
+</script>
+
+<style scoped>
+  #stream1 {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    width: 555px;
+    height: 500px;
+    background-image: url("../../../static/middle-info-background.png");
+    border: 2px var(--container-border-color) solid;
+  }
+  #stream2 {
+    position: absolute;
+    top: 0px;
+    left: 1365px;
+    width: 555px;
+    height: 499px;
+    background-image: url("../../../static/middle-info-background.png");
+    border: 2px var(--container-border-color) solid;
+  }
+  #stream3 {
+    position: absolute;
+    top: 500px;
+    left: 0px;
+    width: 555px;
+    height: 500px;
+    background-image: url("../../../static/middle-info-background.png");
+    border: 2px var(--container-border-color) solid;
+  }
+  #stream4 {
+    position: absolute;
+    top: 500px;
+    left: 1365px;
+    width: 555px;
+    height: 499px;
+    background-image: url("../../../static/middle-info-background.png");
+    border: 2px var(--container-border-color) solid;
+  }
+  #fillbar {
+    position: absolute;
+    top: 1000px;
+    left: 0px;
+    width: 1920px;
+    height: 80px;
+    background-image: url("../../../static/middle-info-background.png");
+    border: 2px var(--container-border-color) solid;
+  }
+  #discord-voice {
+    position: absolute;
+	top: 62px;
+	left: 1056px;
+	width: 309px;
+	height: 438px;
+    background-image: url("../../../static/middle-info-background.png");
+    border: 2px var(--container-border-color) solid;
+  }
+  #InfoStorageBox1 {
+    position: absolute;
+    left: 626px;
+    top: 0px;
+    width: 668px;
+    height: 308px;
+  }
+  .PlayerContainer {
+    width: 580px;
+  }
+  .PlayerContainer >>> .PlayerInfoBox {
+    height: 40px;
+    font-size: 25px;
+  }
+  #PlayerContainer1, #PlayerContainer3 {
+    margin-right: 88px;
+  }
+  #PlayerContainer2, #PlayerContainer4 {
+    margin-top: 3px;
+    margin-left: 88px;
+  }
+  #InfoStorageBox2 {
+    position: absolute;
+    left: 626px;
+    top: 633px;
+    width: 668px;
+    height: 307px;
+  }
+  #pi1 {
+    position: absolute;
+    top: 0px;
+    left: 555px;
+    border: 2px var(--container-border-color) solid;
+    width: 389px;
+  }
+
+  #pi2 {
+	  position: absolute;
+	  top: 0px;
+	  left: 960px;
+	  border: 2px var(--container-border-color) solid;
+	  width: 389px;
+  }
+  #pi3 {
+    position: absolute;
+    top: 938px;
+    left: 555px;
+    border: 2px var(--container-border-color) solid;
+    width: 389px;
+  }
+
+  #pi4 {
+	  position: absolute;
+	  top: 938px;
+	  left: 960px;
+	  border: 2px var(--container-border-color) solid;
+	  width: 389px;
+  }
+  #Bingo-board {
+    position: absolute;
+    top: 500px;
+    left: 555px;
+    border: 2px var(--container-border-color) solid;
+    width:810px;
+    height:438px;
+  }
+  #game {
+    background-image: url("../../../static/middle-info-background.png");
+    position: absolute;
+    top: 62px;
+    left: 555px;
+    width: 501px;
+    border: 2px var(--container-border-color) solid;
+    height: 228px;
+  }
+  #timer{
+    position: absolute;
+    top: 293px;
+    left: 555px;
+    background-image: url("../../../static/middle-info-background.png");
+    width: 501px;
+    border: 2px var(--container-border-color) solid;
+    height: 207px;
+  }
+  #fillupperplayers, #filllowerplayers {
+	  background-image: linear-gradient(var(--alternative-main-color), var(--darker-main-color));
+	  left: 672px;
+	  height: 130px;
+	  width: 581px;
+	  position: absolute;
+  }
+
+  #fillupperplayers {
+	  top: 0px;
+  }
+  #filllowerplayers {
+	  top: 870px;
+  }
+
+</style>
---
+++ src/graphics/game-layout/3_2-3p.vue
@@ -0,0 +1,146 @@
+<template>
+	<div>
+		<twitch-player id="stream1" streamIndex="0"></twitch-player>
+		<twitch-player id="stream2" streamIndex="1"></twitch-player>
+		<twitch-player id="stream3" streamIndex="2"></twitch-player>
+    <div id="fillbar" class="flexContainer"></div>
+    <div id="fillvoice" class="flexContainer"></div>
+    <player-info id="pi1" playerIndex="0" height=45px></player-info>
+    <player-info id="pi2" playerIndex="1" height=45px></player-info>
+    <player-info id="pi3" playerIndex="2" height=45px></player-info>
+		<test-game-container id="game"></test-game-container>
+    <test-timer-container id="timer"></test-timer-container>
+    <bingo-board id="Bingo-board" fontSize="30px"></bingo-board>
+    <discord-voice-display id="discord-voice" iconHeight="40px" nameWidth="125px"/>
+	</div>
+</template>
+
+<script lang="ts">
+	import { Component, Vue, Watch, Prop } from "vue-property-decorator";
+	import { nodecg, NodeCG } from "../../browser-util/nodecg";
+	import { Bingoboard, BingosyncSocket, BingoboardMeta } from "../../../schemas";
+	import { store, getReplicant } from "../../browser-util/state";
+  import TestTimerContainer from "../components/timerContainer.vue";
+	import TestGameContainer from "../components/gameContainer.vue";
+  import BingoBoard from "../components/bingoboard.vue";
+  import PlayerInfo from "../components/playerInfo.vue";
+  import TeamInfo from "../components/teamInfo.vue";
+  import PlayerTeamContainer from "../components/playerTeamContainer.vue";
+  import DiscordVoiceDisplay from "../components/discordVoiceDisplay.vue";
+  import { RunDataPlayer, RunDataTeam } from "../../../speedcontrol-types";
+  import TwitchPlayer from "../components/twitchPlayer.vue";
+
+	@Component({
+		components: {
+			BingoBoard,
+      TestGameContainer,
+      PlayerInfo,
+      TeamInfo,
+      PlayerTeamContainer,
+      TestTimerContainer,
+      DiscordVoiceDisplay,
+			TwitchPlayer
+		}
+	})
+
+	export default class GameLayout extends Vue {
+    get teams(): RunDataTeam[] {
+      return store.state.runDataActiveRun.teams;
+    }
+	}
+</script>
+
+<style scoped>
+  #stream1 {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    width: 640px;
+    height: 427px;
+    background-image: url("../../../static/middle-info-background.png");
+    border: 2px var(--container-border-color) solid;
+  }
+  #stream2 {
+    position: absolute;
+    top: 0px;
+    left: 640px;
+    width: 640px;
+    height: 427px;
+    background-image: url("../../../static/middle-info-background.png");
+    border: 2px var(--container-border-color) solid;
+  }
+  #stream3 {
+    position: absolute;
+    top: 0px;
+    left: 1280px;
+    width: 640px;
+    height: 427px;
+    background-image: url("../../../static/middle-info-background.png");
+    border: 2px var(--container-border-color) solid;
+  }
+  #fillbar {
+    position: absolute;
+    top: 1000px;
+    left: 0px;
+    width: 1920px;
+    height: 80px;
+    background-image: url("../../../static/middle-info-background.png");
+    border: 2px var(--container-border-color) solid;
+  }
+  #discord-voice {
+    position: absolute;
+    top: 487px;
+    left: 1280px;
+    width: 640px;
+    height: 227px;
+    background-image: url("../../../static/middle-info-background.png");
+    border: 2px var(--container-border-color) solid;
+  }
+  #pi1 {
+    position: absolute;
+    top: 427px;
+    left: 0px;
+    border: 2px var(--container-border-color) solid;
+    width: 625px;
+  }
+  #pi2 {
+    position: absolute;
+    top: 427px;
+    left: 640px;
+    border: 2px var(--container-border-color) solid;
+    width: 625px;
+  }
+  #pi3 {
+    position: absolute;
+    top: 427px;
+    left: 1280px;
+    border: 2px var(--container-border-color) solid;
+    width: 625px;
+  }
+  #Bingo-board {
+    position: absolute;
+    top: 487px;
+    left: 640px;
+    border: 2px var(--container-border-color) solid;
+    width: 640px;
+    height: 513px;
+  }
+  #game {
+    background-image: url("../../../static/middle-info-background.png");
+    position: absolute;
+    top: 487px;
+    left: 0px;
+    width: 640px;
+    border: 2px var(--container-border-color) solid;
+    height: 513px;
+  }
+  #timer{
+    position: absolute;
+    top: 714px;
+    left: 1280px;
+    background-image: url("../../../static/middle-info-background.png");
+    width: 640px;
+    border: 2px var(--container-border-color) solid;
+    height: 286px;
+  }
+</style>
