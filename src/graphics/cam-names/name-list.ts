3c30581a19785dfa5be273b0b6620eb3eef20a35
Floha <floha2011@hotmail.de>
2020-05-28 19:21:51 +0200

    Restyle Charity Interview

---
+++ src/graphics/cam-names/name-list.ts
@@ -0,0 +1,12 @@
+/*eslint-disable*/
+import CamNames_1p from "./1p-cam-names.vue";
+import CamNames_2p from "./2p-cam-names.vue";
+import CamNames_3p from "./3p-cam-names.vue";
+import CamNames_4p from "./4p-cam-names.vue";
+
+export {
+    CamNames_1p,
+    CamNames_2p,
+    CamNames_3p,
+    CamNames_4p,
+}
