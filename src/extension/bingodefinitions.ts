22d1dbd8835b39642c7245818e7e3c1d6db6fa28
Floha <floha2011@hotmail.de>
2021-01-09 16:08:10 +0100

    bingodefinitions updated and bot commands changed to point to sms bingo website

--- src/extension/bingodefinitions.ts
+++ src/extension/bingodefinitions.ts
@@ -1,15 +1,17 @@
-const definitions: {[key: string]: string} = {
-  single: 'In single bingo you need to complete a row/column/diagonal with 5 goals to finish',
-  double: 'In double bingo you need to complete 2 rows/columns/diagonals with 5 goals each to finish, they may overlap',
-  triple: 'In triple bingo you need to complete 3 rows/columns/diagonals with 5 goals each to finish, they may overlap',
-  lockout: 'In lockout bingo each goal can only be claimed by the one player clicking it first, the first player locking up the majority of the goals (13) wins',
-  blackout: 'In blackout bingo every goal on the board has to be done',
-  'lockout invasion': 'In lockout invasion bingo the players start marking goals on opposite sides and try to progress toward the opponents side. A goal can only be marked by one player and they can only mark as much goals as they have on the previous row/column on their side: https://imgur.com/2WPnvHQ ',
-  cinco: 'In cinco bingo the players have to get 5 rows/columns/diagonals with 5 goals each to finish, they may overlap',
-  exploration: 'In Exploration Bingo all goals except 2 goals are hidden and can be revealed by completing adjacent goals, blackout means all goals',
-  coop: ', the players of a team share the same color on the board and work together',
-  'double anti': 'In double anti bingo, each player selects a row/column/diagonal for their opponent and chooses an additional line for themselves to complete',
-  'battleships': 'In Battleship bingo, each team places 2,3 and 4 square long battleships on the board, hidden from the other team, which may not overlap. Goal is to sink all of the other teams ships',
+const definitions: { [key: string]: string } = {
+    single: 'In single bingo you need to complete a row/column/diagonal with 5 goals to finish',
+    double: 'In double bingo you need to complete 2 rows/columns/diagonals with 5 goals each to finish, they may overlap',
+    triple: 'In triple bingo you need to complete 3 rows/columns/diagonals with 5 goals each to finish, they may overlap',
+    lockout: 'In lockout bingo each goal can only be claimed by the one player clicking it first, the first player locking up the majority of the goals (13) wins',
+    blackout: 'In blackout bingo every goal on the board has to be done',
+    'lockout invasion': 'In lockout invasion bingo the players start marking goals on opposite sides and try to progress toward the opponents side. A goal can only be marked by one player and they can only mark as much goals as they have on the previous row/column on their side: https://imgur.com/2WPnvHQ ',
+    cinco: 'In cinco bingo the players have to get 5 rows/columns/diagonals with 5 goals each to finish, they may overlap',
+    exploration: 'In Exploration Bingo all goals except 2 goals are hidden and can be revealed by completing adjacent goals, blackout means all goals',
+    draftlockout: 'In addition to normal lockout rules, the players get to draft 5 goals in the beginning, only they are allowed to complete for the first 30 minutes. If these goals are not completed after 30 minutes they become free to claim for everyone. A player may not delay their own draft goals.',
+    connect5: 'A player wins by achieving 13 goals on the card or a single bingo line. Goals in this line may not be delayed.',
+    coop: ', the players of a team share the same color on the board and work together',
+    'double anti': 'In double anti bingo, each player selects a row/column/diagonal for their opponent and chooses an additional line for themselves to complete',
+    'battleships': 'In Battleship bingo, each team places 2,3 and 4 square long battleships on the board, hidden from the other team, which may not overlap. Goal is to sink all of the other teams ships',
 };
 
 export default definitions;
--- src/extension/twitch-chat-bot.ts
+++ src/extension/twitch-chat-bot.ts
@@ -16,7 +16,11 @@ const log = new nodecg.Logger(`${nodecg.bundleName}:twitch-chat-bot`);
 // var chatCommandsRep = nodecg.Replicant('chatCommands', {defaultValue: {}});
 
 // keep track of cooldowns
-const cooldowns = { runner: { lastUsed: 0, cooldown: 15 }, bingo: { lastUsed: 0, cooldown: 15 } };
+const cooldowns = {
+    runner: { lastUsed: 0, cooldown: 15 },
+    bingo: { lastUsed: 0, cooldown: 15 },
+    standings: {lastUsed: 0, cooldown: 15},
+    schedule: {lastUsed: 0, cooldown: 15}};
 // in case the cooldowns need to be adjusted
 /* nodecg.listenFor('setCommandCooldown',data=>{
     if (!data || !data.command || !data.cooldown) {
@@ -113,6 +117,26 @@ if (bundleConfig.twitch && bundleConfig.twitch.enable && bundleConfig.twitch.cha
           }
         }
       }
+
+        if (userCommandName === 'standings' || userCommandName === 'stats') {
+            // check cooldown to not spam chat
+            if (now - cooldowns.standings.lastUsed < cooldowns.standings.cooldown) {
+                return;
+            }
+            cooldowns.standings.lastUsed = now;
+            client.say(channel, "Current Bingo League standings can be found here: https://sms.bingo/standings")
+                .catch((e): void => log.error('', e));
+        }
+
+        if (userCommandName === 'schedule' || userCommandName === 'upcoming') {
+            // check cooldown to not spam chat
+            if (now - cooldowns.standings.lastUsed < cooldowns.standings.cooldown) {
+                return;
+            }
+            cooldowns.standings.lastUsed = now;
+            client.say(channel, "Find the upcoming matches for the league here: https://sms.bingo/schedule")
+                .catch((e): void => log.error('', e));
+        }
       /* also custom chatbot stuff not used
           if (chatCommandsRep.value.hasOwnProperty(userCommandName)) {
               var userCommand = chatCommandsRep.value[userCommandName];
@@ -128,7 +152,7 @@ if (bundleConfig.twitch && bundleConfig.twitch.enable && bundleConfig.twitch.cha
       .catch((e): void => log.error('', e))
       .then((): void => {
         client.on('message', messageHandler);
-        client.join(twichAPIDataRep.value.channelName || 'speedrunslive')
+        client.join(twichAPIDataRep.value.channelName || 'bingothon')
           .catch((reason): void => {
             log.error(`Couldn't join channel: ${reason}`);
           }).then((data): void => {
