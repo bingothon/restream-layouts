a8cc37cbd6fbd4e98753790f6f7eb17cf7f7ea5a
Floha <floha2011@hotmail.de>
2021-01-11 16:15:37 +0100

    interview sunshine ready

--- src/graphics/interview/var-interview.vue
+++ src/graphics/interview/var-interview.vue
@@ -1,22 +1,23 @@
 <template>
-	<div class="Interview">
-		<img src="../../../static/logo-winter-wide.png" id="logo">
-		<div id="fillvoice" class="flexContainer"></div>
-		<div id="team-container" v-if="playerCount.length===4 && teamCount.length===2">
-			<player-team-container v-for="teamIndex in teamCount" :key="teamIndex" class="team" :teamIndex="teamIndex"
-					   height="40px"></player-team-container>
-		</div>
-		<div v-else id="player-container">
-			<player-info v-for="playerIndex in playerCount" :key="playerIndex" class="player" :playerIndex="playerIndex"
-						 height=45px></player-info>
-		</div>
-		<test-game-container id="game"></test-game-container>
-		<test-timer-container id="timer"></test-timer-container>
-		<bingo-board id="Bingo-board" fontSize="20px"></bingo-board>
-		<discord-voice-display id="discord-voice" iconHeight="40px" nameWidth="114px"
-							   maxUserCount="16"></discord-voice-display>
-		<test-next-game-container id="nextGame"></test-next-game-container>
-	</div>
+    <div class="Interview">
+        <div id="fillvoice" class="flexContainer"></div>
+        <div v-if="playerCount.length===4 && teamCount.length===2" id="team-container">
+            <player-team-container v-for="teamIndex in teamCount" :key="teamIndex" :teamIndex="teamIndex" class="team"
+                                   height="40px"></player-team-container>
+        </div>
+        <div v-else id="player-container">
+            <player-info v-for="playerIndex in playerCount" :key="playerIndex" :playerIndex="playerIndex" class="player"
+                         height=45px></player-info>
+        </div>
+        <div id="game" class="flexContainer">
+            <test-game-container id="gamec"></test-game-container>
+            <img id="logo" src="../../../static/Super_Mario_Sunshine_logo.png">
+        </div>
+        <test-timer-container id="timer"></test-timer-container>
+        <bingo-board id="Bingo-board" fontSize="20px"></bingo-board>
+        <discord-voice-display id="discord-voice" iconHeight="40px" maxUserCount="6"
+                               nameWidth="114px"></discord-voice-display>
+    </div>
 </template>
 
 <script lang="ts">
@@ -31,116 +32,122 @@ import PlayerTeamContainer from "../components/playerTeamContainer.vue";
 import DiscordVoiceDisplay from "../components/discordVoiceDisplay.vue";
 
 @Component({
-	components: {
-		BingoBoard,
-		TestGameContainer,
-		PlayerInfo,
-		TeamInfo,
-		PlayerTeamContainer,
-		TestTimerContainer,
-		DiscordVoiceDisplay,
-	}
+    components: {
+        BingoBoard,
+        TestGameContainer,
+        PlayerInfo,
+        TeamInfo,
+        PlayerTeamContainer,
+        TestTimerContainer,
+        DiscordVoiceDisplay,
+    }
 })
 
 export default class Interview extends Vue {
-	get playerCount(): number[] {
-		let count = 0;
-		const playerIndexes = [];
-		store.state.runDataActiveRun.teams.forEach(t => {
-			t.players.forEach(p => {
-				playerIndexes.push(count);
-				count++;
-			});
-		});
-		return playerIndexes;
-	}
+    get playerCount(): number[] {
+        let count = 0;
+        const playerIndexes = [];
+        store.state.runDataActiveRun.teams.forEach(t => {
+            t.players.forEach(p => {
+                playerIndexes.push(count);
+                count++;
+            });
+        });
+        return playerIndexes;
+    }
 
-	get teamCount(): number[] {
-		let count = 0;
-		const teamIndexes = [];
-		store.state.runDataActiveRun.teams.forEach(t => {
-			teamIndexes.push(count);
-			count++;
-		});
-		return teamIndexes;
-	}
+    get teamCount(): number[] {
+        let count = 0;
+        const teamIndexes = [];
+        store.state.runDataActiveRun.teams.forEach(t => {
+            teamIndexes.push(count);
+            count++;
+        });
+        return teamIndexes;
+    }
 }
 </script>
 
 <style scoped>
-.Interview {
-	position: absolute;
-	left: 0px;
-	top: 0px;
-	width: 1935px;
-	height: 1090px;
-	background: linear-gradient(var(--darker-main-color), var(--lighter-main-color));
-}
-
-#discord-voice {
-	position: absolute;
-	top: 500px;
-	left: 1330px;
-	width: 500px;
-	height: 400px;
-	background-color: var(--container-background-color);
-}
-
-#logo {
-	position: absolute;
-	left: 543px;
-	top: 10px;
-	/*width: 568px;*/
-	height: 200px;
-}
-
 #player-container {
-	position: absolute;
-	top: 500px;
-	left: 90px;
+    position: absolute;
+    top: 500px;
+    left: 90px;
 }
 
 #team-container {
-	position: absolute;
-	top: 500px;
-	left: 90px;
+    position: absolute;
+    top: 500px;
+    left: 90px;
 }
 
 .player {
-	width: 500px;
-	margin-bottom: 30px;
+    width: 500px;
+    margin-bottom: 30px;
 }
 
 .team {
-	width: 500px;
-	margin-bottom: 30px;
+    width: 500px;
+    margin-bottom: 30px;
 }
 
+.Interview {
+    position: absolute;
+    left: 0px;
+    top: 0px;
+    width: 1935px;
+    height: 1090px;
+    background-size: cover;
+    background-image: url("../../../static/background-sunshine.jpg");
+    background-repeat: no-repeat;
+}
+#discord-voice {
+    position: absolute;
+    top: 650px;
+    left: 1330px;
+    width: 500px;
+    height: 200px;
+    background-color: var(--container-background-color);
+}
+#logo{
+    position: absolute;
+    left: 690px;
+    top: 10px;
+    width: 568px;
+    height: 200px;
+}
 #Bingo-board {
-	position: absolute;
-	top: 500px;
-	left: 710px;
-	width: 500px;
-	height: 500px;
+    position: absolute;
+    top: 500px;
+    left: 710px;
+    width: 500px;
+    height: 500px;
 }
-
 #game {
-	position: absolute;
-	top: 225px;
-	left: 460px;
-	width: 1000px;
-	height: 100px;
+    position: absolute;
+    top: 50px;
+    left: 460px;
+    width: 1000px;
+    height: 100px;
 }
-
-#game.GameContainer > .GameExtra {
-	color: #484848;
+#gamec {
+    position: absolute;
+    left: 0px;
+    width: 1000px;
+    top: 200px;
 }
-
-#timer {
-	position: absolute;
-	top: 325px;
-	left: 710px;
-	width: 500px;
-	height: 150px;
+#logo {
+    position: absolute;
+    height: 150px;
+    width: 255px;
+    top: 25px;
+    left: 370px;
+}
+#timer{
+    position: absolute;
+    top: 325px;
+    left: 710px;
+    width: 500px;
+    height: 150px;
 }
 </style>
