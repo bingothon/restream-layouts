6389743192721fa2845385edd37e4a6beef0873d
Floha <floha2011@hotmail.de>
2021-07-28 01:42:33 +0200

    Fix sms.bingo import

--- src/dashboard/smsBingoImport/main.vue
+++ src/dashboard/smsBingoImport/main.vue
@@ -1,5 +1,10 @@
 <template>
     <v-app>
+        <span>Import Runs for Channel</span>
+        <v-select
+            :items="ALL_CHANNELS"
+            v-model="channel">
+        </v-select>
         <v-btn
             class="green darken-2"
             @click="importAll"
@@ -9,24 +14,18 @@
             </v-icon>
             Import from sms.bingo
         </v-btn>
-        <v-btn
-            class="red darken-2 mt-3"
-            @click="removeAllRuns"
-        >
-            <v-icon class="pr-2">
-                mdi-delete
-            </v-icon>
-            Remove All Runs
-        </v-btn>
     </v-app>
 </template>
 
 <script lang="ts">
 import {Component, Vue} from "vue-property-decorator";
-import { nodecg } from '../../browser-util/nodecg';
+import {nodecg} from '../../browser-util/nodecg';
 
 @Component({})
 export default class SMSBingoImport extends Vue {
+
+    ALL_CHANNELS = ["Bingothon", "SunshineCommunity"];
+    channel: string = 'Bingothon';
 
     async removeAllRuns(confirm: boolean): Promise<void> {
         if (confirm) {
@@ -47,7 +46,7 @@ export default class SMSBingoImport extends Vue {
     async importRuns(): Promise<void> {
         console.log("Started Import Function")
         try {
-            await nodecg.sendMessage('importSMSBingo')
+            await nodecg.sendMessage('importSMSBingo', {channel: this.channel})
             console.log('Sent Import Message')
         } catch (err) {
             // unsuccessful
