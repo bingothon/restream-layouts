46855d52843d24b9219e2132253c41c5a999d357
Floha <floha2011@hotmail.de>
2022-01-25 01:47:53 +0100

    fix BoX totalGames not updating

--- src/dashboard/bestOfX-control/main.vue
+++ src/dashboard/bestOfX-control/main.vue
@@ -8,16 +8,16 @@
         ></v-checkbox>
         <span>Total Amount of Matches</span>
         <v-text-field
-            v-model="totalMatches"
+            :value="totalMatches"
+            @change="updateTotal"
             background-color="#455A64"
             class="score-count"
             dark
             type="number"
-            @change="updateTotal"
         />
         <div v-if="enabled"
-             v-for="(count,i) in teams"
-             :key="i"
+             v-for="(team,i) in teams"
+             :key="team, i"
         >
             <span>Player {{ i }} won Matches</span>
             <v-text-field
@@ -36,11 +36,11 @@
 import {Component, Vue} from 'vue-property-decorator';
 import {getReplicant, store} from '../../browser-util/state';
 import {BestOfX} from "../../../schemas";
-import {RunDataTeam} from "../../../speedcontrol-types";
 
 
 @Component({})
 export default class BingoControl extends Vue {
+    total: string = this.totalMatches;
 
     // --- computed properties
     get enabled(): boolean {
@@ -59,12 +59,12 @@ export default class BingoControl extends Vue {
         return store.state.bestOfX.matchCounts.map(i => `${i}`);
     }
 
-    get activated(): boolean {
-        return this.enabled ? this.enabled : false;
-    }
-
-    get teams(): RunDataTeam[] {
-        return store.state.runDataActiveRun.teams;
+    get teams(): string[] {
+        let ids = [];
+        store.state.runDataActiveRun.teams.forEach((team) => {
+            ids.push(team.id)
+        });
+        return ids;
     }
 
     updateMatchScores() {
@@ -77,8 +77,8 @@ export default class BingoControl extends Vue {
         getReplicant<BestOfX>('bestOfX').value.enabled = !this.enabled;
     }
 
-    updateTotal() {
-        getReplicant<BestOfX>('bestOfX').value.totalMatches = parseInt(this.totalMatches, 10);
+    updateTotal(newVal: string) {
+        getReplicant<BestOfX>('bestOfX').value.totalMatches = parseInt(newVal, 10);
     }
 }
 </script>
