d8cf94e69d4741920bdcf90ddd1a5df589059f99
Floha <floha2011@hotmail.de>
2024-04-27 16:19:19 +0200

    formatting

--- src/extension/twitchEventSub.ts
+++ src/extension/twitchEventSub.ts
@@ -1,21 +1,18 @@
-import { ApiClient } from "@twurple/api";
+import { ApiClient } from '@twurple/api';
 import { RefreshingAuthProvider } from '@twurple/auth';
-import {
-    EventSubHttpListener,
-    // ReverseProxyAdapter
-} from '@twurple/eventsub-http';
-import { NgrokAdapter} from '@twurple/eventsub-ngrok';
-import {Configschema} from '@/configschema';
-import * as nodecgApiContext from "./util/nodecg-api-context";
-import { sentenceCase } from "sentence-case";
+import { EventSubHttpListener } from '@twurple/eventsub-http';
+import { NgrokAdapter } from '@twurple/eventsub-ngrok';
+import { Configschema } from '@/configschema';
+import * as nodecgApiContext from './util/nodecg-api-context';
+import { sentenceCase } from 'sentence-case';
 import { EventSubChannelFollowEvent, EventSubChannelSubscriptionEvent } from '@twurple/eventsub-base';
 
-let isRunningQueue = false
+let isRunningQueue = false;
 const eventsQueue: { type: string; data: EventSubChannelFollowEvent | EventSubChannelSubscriptionEvent }[] = [];
-let waitForNext = false
+let waitForNext = false;
 
 // bingothon const userId = '539920154';
-const userId = '53717309' //Floha258
+const userId = '53717309'; //Floha258
 const nodecg = nodecgApiContext.get();
 const config = nodecg.bundleConfig as Configschema;
 
@@ -23,9 +20,9 @@ const clientId = config.twitchEventSub?.clientID || '';
 const clientSecret = config.twitchEventSub?.clientSecret || '';
 
 const authProvider = new RefreshingAuthProvider({ clientId, clientSecret });
-const client = new ApiClient({authProvider});
+const client = new ApiClient({ authProvider });
 
-const logger = new (nodecgApiContext.get()).Logger('eventSub')
+const logger = new (nodecgApiContext.get().Logger)('eventSub');
 
 // logger.info('Auth Provider Scopes', authProvider.currentScopes);
 
@@ -49,83 +46,84 @@ const listener = new EventSubHttpListener({
         hostName: config.twitchEventSub.hostName, // The host name the server is available from
         port: 443 // The external port (optional, defaults to 443)
     }),*/
-    adapter: new NgrokAdapter({ngrokConfig: { authtoken: '2fewqxu78HjNhyyALMttGM6RbDc_4aQ4Tyt1zeDQapeGE4ot' }}),
+    adapter: new NgrokAdapter({ ngrokConfig: { authtoken: '2fewqxu78HjNhyyALMttGM6RbDc_4aQ4Tyt1zeDQapeGE4ot' } }),
     secret: config.twitchEventSub?.eventSubListenerKey || ''
 });
 
-const getChannelEventSubscriptions = (obj: object) : string[] => {
-    const properties = new Set<string>()
-    let currentObj = obj
+const getChannelEventSubscriptions = (obj: object): string[] => {
+    const properties = new Set<string>();
+    let currentObj = obj;
     do {
-        Object.getOwnPropertyNames(currentObj).map(item => properties.add(item))
-    } while ((currentObj = Object.getPrototypeOf(currentObj)))
-    return [...properties.keys()].filter(item => item.includes('subscribeToChannel'))
-}
+        Object.getOwnPropertyNames(currentObj).map((item) => properties.add(item));
+    } while ((currentObj = Object.getPrototypeOf(currentObj)));
+    return [...properties.keys()].filter((item) => item.includes('subscribeToChannel'));
+};
 
-const delay = (milliseconds: number) => new Promise(resolve => { setTimeout(resolve, milliseconds) })
+const delay = (milliseconds: number) =>
+    new Promise((resolve) => {
+        setTimeout(resolve, milliseconds);
+    });
 
 const queueSendHandler = async (waitForNext: boolean) => {
-
     // eventsQueue[0].data has EventSubHandler object attached with it's own properties.
     // You'd actaully want to forward all as in `data: eventsQueue[0].data`
     // then in the vue file, use the property that is suitable for information you'd need.
     // see, twurple documentation on EventSubHandler for properties specific to eventsubscription types.
 
     //console.log("Checking for new Subs and Follows")
-    if(!waitForNext){
-        while(eventsQueue.length > 0){
+    if (!waitForNext) {
+        while (eventsQueue.length > 0) {
             isRunningQueue = true;
-            await delay(10000)
-            nodecg.sendMessage(`${eventsQueue[0].type}`, {username: eventsQueue[0].data.userDisplayName})
-            await delay(10000)
-            eventsQueue.shift()
+            await delay(10000);
+            nodecg.sendMessage(`${eventsQueue[0].type}`, { username: eventsQueue[0].data.userDisplayName });
+            await delay(10000);
+            eventsQueue.shift();
         }
         isRunningQueue = false;
     }
-}
+};
 
-const newEventCheck = async() => {
+const newEventCheck = async () => {
     waitForNext = isRunningQueue;
-    await queueSendHandler(waitForNext)
-}
+    await queueSendHandler(waitForNext);
+};
 
 const main = async () => {
-    const eventSubscriptions = getChannelEventSubscriptions(listener)
-    for(const eventSubscription of eventSubscriptions){
+    const eventSubscriptions = getChannelEventSubscriptions(listener);
+    for (const eventSubscription of eventSubscriptions) {
         try {
-            const parsedEventType = sentenceCase(eventSubscription.slice(11, eventSubscription.length))
-                .replace(/ /g, "_").toUpperCase()
-            if(parsedEventType === 'CHANNEL_FOLLOW_EVENTS'){
-                console.log(`Attempting to subscribe to ${parsedEventType}`)
-                listener[(eventSubscription as 'onChannelFollow')](userId, userId, (event) => {
-                    console.log(`what is here`, typeof event)
+            const parsedEventType = sentenceCase(eventSubscription.slice(11, eventSubscription.length)).replace(/ /g, '_').toUpperCase();
+            if (parsedEventType === 'CHANNEL_FOLLOW_EVENTS') {
+                console.log(`Attempting to subscribe to ${parsedEventType}`);
+                listener[eventSubscription as 'onChannelFollow'](userId, userId, (event) => {
+                    console.log(`what is here`, typeof event);
                     eventsQueue.push({
                         type: parsedEventType,
                         data: event
-                    })
+                    });
                 });
             }
-            if(parsedEventType === 'CHANNEL_SUBSCRIBTION_EVENTS') {
-                console.log(`Attempting to subscribe to ${parsedEventType}`)
-                listener[(eventSubscription as 'onChannelSubscription')](userId, (event) => {
-                    console.log(`what is here`, typeof event)
+            if (parsedEventType === 'CHANNEL_SUBSCRIBTION_EVENTS') {
+                console.log(`Attempting to subscribe to ${parsedEventType}`);
+                listener[eventSubscription as 'onChannelSubscription'](userId, (event) => {
+                    console.log(`what is here`, typeof event);
                     eventsQueue.push({
                         type: parsedEventType,
                         data: event
-                    })
+                    });
                 });
             }
-        }catch(err){
-            console.log(err)
+        } catch (err) {
+            console.log(err);
         }
     }
     listener.start();
-    
-    const channelSubscriptionEvent = listener.onChannelSubscription(userId, event => console.log(`sub by ${event.userDisplayName}`));
-    const channelFollowEvent = listener.onChannelFollow(userId, userId,event => console.log(`follow by ${event.userDisplayName}`));
-    logger.info('sub', await channelSubscriptionEvent.getCliTestCommand())
-    logger.info('follow', await channelFollowEvent.getCliTestCommand())
-    setInterval(newEventCheck, 1000)
-}
 
-main()
+    const channelSubscriptionEvent = listener.onChannelSubscription(userId, (event) => console.log(`sub by ${event.userDisplayName}`));
+    const channelFollowEvent = listener.onChannelFollow(userId, userId, (event) => console.log(`follow by ${event.userDisplayName}`));
+    logger.info('sub', await channelSubscriptionEvent.getCliTestCommand());
+    logger.info('follow', await channelFollowEvent.getCliTestCommand());
+    setInterval(newEventCheck, 1000);
+};
+
+main();
